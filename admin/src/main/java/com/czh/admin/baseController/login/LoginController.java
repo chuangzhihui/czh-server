c4a7dee210fad6e5f04c995a2d9bee891fe2d522
17059566269 <1137805463@qq.com>
2026-01-13 21:00:02 +0800

    增加前端获取上传token的接口

--- admin/src/main/java/com/czh/admin/baseController/login/LoginController.java
+++ admin/src/main/java/com/czh/admin/baseController/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.czh.admin.baseController.login;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.czh.admin.baseController.BaseController;
 import com.czh.admin.security.AdminDetails;
 import com.czh.common.annotation.Log;
@@ -8,11 +10,15 @@ import com.czh.common.utils.DateUtil;
 import com.czh.common.utils.JWTUtil;
 import com.czh.common.utils.RequestUtil;
 import com.czh.common.utils.ValidateCodeUtil;
+import com.czh.common.utils.tosUtil.TosUtil;
+import com.czh.common.utils.tosUtil.entity.GetTosSignUrlDto;
+import com.czh.common.utils.tosUtil.entity.TosEntity;
 import com.czh.common.vo.JSONResult;
 import com.czh.service.dto.admin.AdminLoginDto;
 import com.czh.service.entity.Admin;
 import com.czh.service.entity.AuthConfig;
 import com.czh.service.entity.Setting;
+import com.czh.service.entity.UploadSet;
 import com.czh.service.vo.admin.AdminLoginVo;
 import com.czh.service.vo.admin.GetSystemNameVo;
 import com.czh.service.vo.admin.GetVerifyVo;
@@ -28,6 +34,7 @@ import javax.imageio.ImageIO;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -122,4 +129,29 @@ public class LoginController extends BaseController {
 
     }
 
+
+    /**
+     * 获取当前的上传配置信息
+     * @return
+     */
+    @RequestMapping("/getUploadToken")
+    public JSONResult<Map<String,Object>> getUploadToken(){
+        Map<String,Object> config=uploadSetService.getUploadToken();
+        return JSONResult.success(config);
+    }
+
+    /**
+     * 获取火山云的签名地址
+     * @param req
+     * @return
+     */
+    @PostMapping("/getTosSignUrl")
+    public JSONResult<Map<String,Object>>getTosSignUrl(@RequestBody GetTosSignUrlDto req)
+    {
+        UploadSet set=uploadSetService.getById(1);
+        TosEntity tosEntity= JSON.toJavaObject(JSONObject.parseObject(set.getTos()),TosEntity.class);
+        TosUtil tosUtil=new TosUtil(tosEntity);
+        Map<String,Object> map=tosUtil.getUploadUrl(req.getKey());
+        return JSONResult.success(map);
+    }
 }
